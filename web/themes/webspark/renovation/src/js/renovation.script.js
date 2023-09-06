33d181008757688f953ba887b378abc5516a7b52
Alexander Persky <apersky@asu.edu>
2023-09-06 15:20:11 -0700

    Add example behavior to the Renovation script file
    - I usually am against leaving files like this in the codebase, but having our own JS here is common so I think its ok to stay

---
+++ web/themes/webspark/renovation/src/js/renovation.script.js
@@ -0,0 +1,21 @@
+// Example Drupal behavior
+// (function ($, Drupal) {
+//   "use strict";
+
+//   Drupal.behaviors.renovation = {
+//     attach: function (context, settings) {
+//       $(".el", context)
+//         .once("renovation")
+//         .each(function () {
+//           console.log("Renovation behavior attached.");
+//         });
+//     },
+//     detach: function (context, settings, trigger) {
+//       $(".el", context)
+//         .removeOnce("renovation")
+//         .each(function () {
+//           console.log("Renovation behavior detacthed.");
+//         });
+//     },
+//   };
+// })(jQuery, Drupal);
