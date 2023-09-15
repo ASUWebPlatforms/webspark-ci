cf5a23c91d246891e2a7b0b828739a9a9c36d695
Juan Pablo Mitriatti <juan.mitriatti@gmail.com>
2023-09-15 11:54:00 -0300

    WS2-1452 : Refactoring table. Now using the default table provided by ckeditor5.

--- web/modules/webspark/webspark_ckeditor_plugins/js/ckeditor5_plugins/websparkPlugin/src/table/inserttablecommand.js
+++ web/modules/webspark/webspark_ckeditor_plugins/js/ckeditor5_plugins/websparkPlugin/src/table/inserttablecommand.js
@@ -1,39 +1,50 @@
-/**
- * @file defines InsertWebsparkTableCommand, which is executed when the websparkTable
- * form is submitted.
- */
-
 import { Command } from "ckeditor5/src/core";
-import TableUtils from "@ckeditor/ckeditor5-table/src/tableutils";
-import { TableToolbar } from "@ckeditor/ckeditor5-table";
 
 export default class InsertWebsparkTableCommand extends Command {
-  execute({ rows, cols, headers, tabletype, caption }) {
+  execute({ rows, cols: columns, headers, tabletype, caption }) {
     const { model } = this.editor;
-    let rowsTable = parseInt(rows, 10) || 1;
-    let colTables = parseInt(cols, 10) || 1;
-    let headersTable = headers;
-    let captionTable = caption || "";
     const tableUtils = this.editor.plugins.get("TableUtils");
-    const selection = this.editor.model.document.selection;
+
+    const headingRows = headers === "both" || headers === "row" ? 1 : 0;
+    const headingColumns = headers === "both" || headers === "column" ? 1 : 0;
+
     model.change((writer) => {
-      const insertAbove = false;
-      const affectedTableCells =
-        tableUtils.getSelectionAffectedTableCells(selection);
-    
-      const isHeadingColumns = headersTable !== "none" ? 1 : 0;
       const table = tableUtils.createTable(writer, {
-        rows: 1,
-        headingColums: isHeadingColumns,
-        headingRows: isHeadingColumns,
-        columns: 5,
+        rows,
+        columns,
+        headingRows,
+        headingColumns,
       });
-      
+
       model.insertObject(table, null, null, { findOptimalPosition: "auto" });
-      // Placing the cursor on the first element.
-      writer.setSelection(
-        writer.createPositionAt(table.getNodeByPath([0, 0, 0]), 0)
+      // Set the cursor at the beginning of the table
+      writer.setSelection(writer.createPositionAt(table.getNodeByPath([0]), 0));
+
+      // Create a range around the table
+      const range = writer.createRange(
+        writer.createPositionBefore(table),
+        writer.createPositionAfter(table)
       );
+      // Create a wrapper element for the table
+      const wrapper = writer.createElement("websparkTable", {
+        type: tabletype,
+      });
+
+      // Wrap the range with the wrapper element and append the table
+      writer.wrap(range, wrapper);
+      writer.append(table, wrapper);
+      model.insertContent(wrapper);
+
+      // Check if a caption is provided and insert it if it exists
+      if (caption?.length) {
+        const newCaptionElement = writer.createElement("caption");
+        writer.insertText(caption, newCaptionElement, "end");
+
+        const tableElement = getSelectionAffectedTable(
+          model.document.selection
+        );
+        model.insertContent(newCaptionElement, tableElement, "end");
+      }
     });
   }
 
@@ -41,30 +52,21 @@ export default class InsertWebsparkTableCommand extends Command {
     const { model } = this.editor;
     const { selection } = model.document;
 
-    // Determine if the cursor (selection) is in a position where adding a
-    // websparkTable is permitted. This is based on the schema of the model(s)
-    // currently containing the cursor.
     const allowedIn = model.schema.findAllowedParent(
       selection.getFirstPosition(),
       "websparkTable"
     );
 
-    // If the cursor is not in a location where a simpleBox can be added, return
-    // null so the addition doesn't happen.
     this.isEnabled = allowedIn !== null;
-
-    const selectedElement = selection.getSelectedElement();
-
-    if (selectedElement?.name === "websparkTable") {
-      const text = selectedElement?.getChild(0)?.getChild(0)?._data;
-
-      model.change((writer) => {
-        writer.setAttribute("text", text, selectedElement);
-      });
-
-      this.value = Object.fromEntries(selectedElement.getAttributes());
-    } else {
-      this.value = null;
-    }
   }
+}
+function getSelectionAffectedTable(selection) {
+  const selectedElement = selection.getSelectedElement();
+
+  // Is the command triggered from the `tableToolbar`?
+  if (selectedElement && selectedElement.is("element", "table")) {
+    return selectedElement;
+  }
+
+  return selection.getFirstPosition().findAncestor("table");
 }
--- web/modules/webspark/webspark_ckeditor_plugins/js/ckeditor5_plugins/websparkPlugin/src/table/websparktableediting.js
+++ web/modules/webspark/webspark_ckeditor_plugins/js/ckeditor5_plugins/websparkPlugin/src/table/websparktableediting.js
@@ -1,6 +1,3 @@
-import { Plugin } from "ckeditor5/src/core";
-import { Widget, toWidget, toWidgetEditable } from "ckeditor5/src/widget";
-import InsertWebsparkTableCommand from "./inserttablecommand";
 import {
   PlainTableOutput,
   Table,
@@ -10,27 +7,10 @@ import {
   TableToolbar,
   TableUtils,
 } from "@ckeditor/ckeditor5-table";
+import { Plugin } from "ckeditor5/src/core";
+import { Widget, toWidget } from "ckeditor5/src/widget";
+import InsertWebsparkTableCommand from "./inserttablecommand";
 
-// cSpell:ignore simplebox insertsimpleboxcommand
-
-/**
- * CKEditor 5 plugins do not work directly with the DOM. They are defined as
- * plugin-specific data models that are then converted to markup that
- * is inserted in the DOM.
- *
- * CKEditor 5 internally interacts with simpleBox as this model:
- * <websparkTable>
- *    <websparkTableText></websparkTableText>
- * </websparkTable>
- *
- * Which is converted for the browser/user as this markup
- * <a class="btn">
- *   <span class="text"></span>
- * </a>
- *
- * This file has the logic for defining the simpleBox model, and for how it is
- * converted to standard DOM markup.
- */
 export default class WebsparkTableEditing extends Plugin {
   static get requires() {
     return [
@@ -54,242 +34,50 @@ export default class WebsparkTableEditing extends Plugin {
     );
   }
 
-  /*
-   * This registers the structure that will be seen by CKEditor 5 as
-   * <websparkTable>
-   *    <websparkTableText></websparkTableText>
-   * </websparkTable>
-   *
-   * The logic in _defineConverters() will determine how this is converted to
-   * markup.
-   */
   _defineSchema() {
-    // Schemas are registered via the central `editor` object.
     const schema = this.editor.model.schema;
 
     schema.register("websparkTable", {
       isObject: true,
       allowWhere: "$block",
-      allowAttributes: ["tabletype"],
-    });
-
-    schema.register("websparkTableHtmlElement", {
-      isLimit: true,
-      allowIn: "websparkTable",
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableCaption", { 
-      isLimit: true,
-      allowIn: "websparkTableHtmlElement",
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableTHead", {
-      isLimit: true,
-      allowIn: "websparkTableHtmlElement",
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableTBody", {
-      isLimit: true,
-      allowIn: "websparkTableHtmlElement",
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableTR", {
-      isLimit: true,
-      allowIn: ["websparkTableTBody", "websparkTableTHead"],
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableTD", {
-      isLimit: true,
-      allowIn: "websparkTableTR",
-      allowContentOf: "$block",
-    });
-
-    schema.register("websparkTableTH", {
-      isLimit: true,
-      allowIn: "websparkTableTR",
-      allowContentOf: "$block",
+      allowChildren: ["table"],
+      allowAttributes: ["type"],
     });
   }
 
-  /**
-   * Converters determine how CKEditor 5 models are converted into markup and
-   * vice-versa.
-   */
   _defineConverters() {
-    // Converters are registered via the central editor object.
     const { conversion } = this.editor;
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableTH",
-      view: {
-        name: "th",
-      },
-    });
 
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableTH",
-      view: {
-        name: "th",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableTH",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("th", {});
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableTD",
-      view: {
-        name: "td",
-      },
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableTD",
-      view: {
-        name: "td",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableTD",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("td", {});
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableTR",
-      view: {
-        name: "tr",
-      },
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableTR",
-      view: {
-        name: "tr",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableTR",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("tr", {});
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableTHead",
-      view: {
-        name: "thead",
-      },
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableTHead",
-      view: {
-        name: "thead",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableTHead",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("thead", {});
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableTBody",
-      view: {
-        name: "tbody",
-      },
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableTBody",
-      view: {
-        name: "tbody",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableTBody",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("tbody", {});
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableCaption",
-      view: {
-        name: "caption",
-      },
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableCaption",
-      view: {
-        name: "caption",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableCaption",
-      view: (_modelElement, { writer }) => {
-        const paragraphElement = writer.createEditableElement("caption", {});
-
-        return toWidgetEditable(paragraphElement, writer);
-      },
-    });
-
-    conversion.for("upcast").elementToElement({
-      model: "websparkTableHtmlElement",
-      view: {
-        name: "table",
-        classes: "",
-      },
-      converterPriority: "high",
-    });
-
-    conversion.for("dataDowncast").elementToElement({
-      model: "websparkTableHtmlElement",
-      view: {
-        name: "table",
-        classes: "",
-      },
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "websparkTableHtmlElement",
-      view: (_modelElement, { writer }) => {
-        const table = writer.createEditableElement("table", {
-          class: "",
-        });
-        return toWidgetEditable(table, writer);
-      },
-    });
+    // Override default table data downcast converter.
+		/*conversion.for( 'dataDowncast' ).elementToStructure( {
+			model: 'table',
+			view: downcastTableElement,
+			converterPriority: 'high'
+		} );*/
+ 
+// editingDowncast for editing pipeline
+/*conversion.for( 'editingDowncast' ).elementToElement( {
+        model: 'table',
+        view: downcastTableCustom,
+       view: {
+            name: 'table',
+            classes: 'paragraph-in-editing-view'
+        },
+        converterPriority: 'high'
+    } );*/
 
     conversion.for("upcast").elementToElement({
       view: {
         name: "div",
-        classes: ["uds-table", /^(uds-table-fixed|\s*)$/],
+        classes: ["uds-table"],
+        attribute: { type: true },
       },
       model: (viewElement, { writer }) => {
+        const classes = viewElement.getAttribute("class");
+        const type = classes.includes("-fixed") ? "fixed" : "default";
+
         return writer.createElement("websparkTable", {
-          tabletype: viewElement.getAttribute("tabletype"),
+          type,
         });
       },
     });
@@ -297,13 +85,14 @@ export default class WebsparkTableEditing extends Plugin {
     conversion.for("dataDowncast").elementToElement({
       model: "websparkTable",
       view: (modelElement, { writer }) => {
-        const tabletypeClass =
-          modelElement.getAttribute("tabletype") === "fixed"
-            ? "uds-table-fixed"
-            : "";
+        let _class = "uds-table";
+
+        if (modelElement.getAttribute("type") === "fixed") {
+          _class += ` uds-table-fixed`;
+        }
+
         return writer.createContainerElement("div", {
-          class: `uds-table ${tabletypeClass}`,
-          tabletype: modelElement.getAttribute("tabletype"),
+          class: _class,
         });
       },
     });
@@ -311,15 +100,9 @@ export default class WebsparkTableEditing extends Plugin {
     conversion.for("editingDowncast").elementToElement({
       model: "websparkTable",
       view: (modelElement, { writer }) => {
-        const tabletypeClass =
-          modelElement.getAttribute("tabletype") === "fixed"
-            ? "uds-table-fixed"
-            : "";
-        const div = writer.createContainerElement("div", {
-          class: `uds-table ${tabletypeClass}`,
-        });
+        const wrapper = writer.createContainerElement("div");
 
-        return toWidget(div, writer, { label: "Webspark Table" });
+        return toWidget(wrapper, writer, { label: "Webspark Table" });
       },
     });
   }
