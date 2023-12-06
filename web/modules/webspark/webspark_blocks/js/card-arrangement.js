904afb8b651572b231fabf825f04298be24022fb
Juan Pablo Mitriatti <juan.mitriatti@gmail.com>
2023-12-06 11:00:18 -0300

    WS2-1847 : Refactoring code. Some validations and comments were added.

--- web/modules/webspark/webspark_blocks/js/card-arrangement.js
+++ web/modules/webspark/webspark_blocks/js/card-arrangement.js
@@ -1,27 +1,33 @@
 (function ($, Drupal, drupalSettings, once) {
+  // Define a behavior for handling card arrangement alerts
   Drupal.behaviors.cardArrangementAlert = {
     attach: function (context, settings) {
-      //Init variables
-      const selectCountColumn =  document.querySelector('[name="settings[block_form][field_card_arrangement_display]"]');
+      // Initialize variables
+      const selectCountColumn = document.querySelector('[name="settings[block_form][field_card_arrangement_display]"]');
       const displayOrientation = document.querySelector('[name="settings[block_form][field_display_orientation]"]');
       const displayDiv = document.querySelector('[data-drupal-selector="edit-settings-block-form-field-display-orientation-wrapper"]');
 
-
-      //Get the element in order to identify Ranking cards
+      // Get the element to identify Ranking cards
       const element = document.querySelector('[name*="[field_card_ranking_image_size]"]');
+      // Check if the element exists to determine display orientation
       if (element) {
+        // Hide display orientation
         _showDisplayOrientation('none');
       } else {
+        // Show display orientation
         _showDisplayOrientation();
       }
 
+      if (settings && settings.columns_values && Array.isArray(settings.columns_values)) {
+        // Initial setup of options and event listener for display orientation change
+        _updateColumnsOptions();
+        displayOrientation.addEventListener('change', _updateColumnsOptions);
+      }
 
-      _removedColumnsOptions();
-      displayOrientation.addEventListener('change', _removedColumnsOptions);
-
-      //Remove opctions in Columns to Display select
-      function _removedColumnsOptions() {
+      // Function to update options in Columns to Display select based on display orientation
+      function _updateColumnsOptions() {
         if (displayOrientation && displayOrientation.value === 'horizontal') {
+          // Remove options when display orientation is horizontal
           settings.columns_values.forEach(option => {
             const key = Object.keys(option)[0];
             for (let i = selectCountColumn.options.length - 1; i >= 0; i--) {
@@ -31,29 +37,32 @@
             }
           });
         } else {
+          // Add options when display orientation is not horizontal
           settings.columns_values.forEach(option => {
             const key = Object.keys(option)[0];
             const value = option[key];
-            if (!Array.from(selectCountColumn.options).some(option => option.value === key)) {
+            if (!Array.from(selectCountColumn.options).some(opt => opt.value === key)) {
               const newOption = document.createElement('option');
               newOption.value = key;
               newOption.text = value;
               selectCountColumn.add(newOption);
             }
-          })
+          });
         }
-        selectCountColumn.value = "''";
+        selectCountColumn.value = "''"; // Reset selected value
       }
 
-      //Show or not Display orientation select
+      // Function to show or hide Display orientation select
       function _showDisplayOrientation(value = '') {
         if (displayDiv) {
-          displayDiv.style.display = value
+          // Set display style based on value
+          displayDiv.style.display = value;
         }
         if (value !== '') {
+           // Set display orientation values
           displayOrientation.value = 'vertical';
         }
       }
     }
-  }
+  };
 })(jQuery, Drupal, drupalSettings, once);
