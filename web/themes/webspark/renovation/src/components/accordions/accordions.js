ee11d260683edb0f24838264b3411f9d66dcfaf3
Bob-E <rmlebla1@asu.edu>
2023-12-08 06:55:02 -0700

    Revert "default open accordion template, accordions.js, and renovation_preprocess_block__inline_block__accordion in block.inc"
    This reverts commit 1824c1a7123fb2285a4bf817d51b9a78b16258bc.

--- web/themes/webspark/renovation/src/components/accordions/accordions.js
+++ web/themes/webspark/renovation/src/components/accordions/accordions.js
@@ -29,39 +29,3 @@
     toggler(window.location.href, true);
   });
 })();
-
-
-// finds Accordions configured to have first accordion panel open by default
-document.addEventListener('DOMContentLoaded', function() {
-  // Select div elements with class "accordion" and data-first-expanded attribute equal to 1
-  var accordionElements = document.querySelectorAll('div.accordion[data-first-expanded="1"]');
-
-  // Iterate through the selected elements
-  accordionElements.forEach(function(accordionElement) {
-    // Find the first child div with class "accordion-item"
-    var accordionItem = accordionElement.querySelector('div.accordion-item');
-
-    // Check if an accordion item is found
-    if (accordionItem) {
-      // Find a tag inside accordionItem with id starting with "accordion-header-"
-      var headerTag = accordionItem.querySelector('[id^="accordion-header-"]');
-      // Find a tag inside accordionItem with id starting with "accordion-content-"
-      var contentTag = accordionItem.querySelector('[id^="accordion-content-"]');
-
-      // Check if the header tag is found
-      if (headerTag) {
-        // Remove the class "collapsed" from the header tag
-        headerTag.classList.remove('collapsed');
-        // Find and update the aria-expanded attribute
-        if (headerTag.getAttribute('aria-expanded') === 'false') {
-          headerTag.setAttribute('aria-expanded', 'true');
-        }
-      }
-      // Check if the content tag is found
-      if (contentTag) {
-        // Add the class "show" to the content tag
-        contentTag.classList.add('show');
-      }
-    }
-  });
-});
