7cdba4ba872524128b1df8b0493df31c5974e34b
David Levy <dlevy4@asu.edu>
2024-02-02 09:38:55 -0700

    Added various Properties, Enums and Classes to support Cards.  Added comment placeholders for Cards props to add.

---
+++ web/modules/webspark/webspark_view_modes/src/Plugin/views/style/SpacingType.php
@@ -0,0 +1,22 @@
+<?php
+
+// Top or Bottom Spacing options in Pixels
+enum SpacingType: String {
+  case None = "- None -";
+  case Eight = "8px";
+  case Sixteen = "16px";
+  case TwentyFour = "24px";
+  case ThirtyTwo = "32px";
+  case Forty = "40px";
+  case FortyEight = "48px";
+  case SeventyTwo = "72px";
+  case NinetySix = "96px";
+  case NegativeEight = "-8px";
+  case NegativeSixteen = "-16px";
+  case NegativeTwentyFour = "-24px";
+  case NegativeThirtyTwo = "-32px";
+  case NegativeForty = "-40px";
+  case NegativeFortyEight = "-48px";
+  case NegativeSeventyTwo = "-72px";
+  case NegativeNinetySix = "-96px";
+}
---
+++ web/modules/webspark/webspark_view_modes/src/Plugin/views/style/ViewModeType.php
@@ -0,0 +1,7 @@
+<?php
+
+// View Mode Types available
+enum ViewModeType: String {
+  case Default = "Default";
+  case Vertical = "Vertical";
+}
