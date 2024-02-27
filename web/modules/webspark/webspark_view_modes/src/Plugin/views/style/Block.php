608bd698452dc892f4b3f94e23cbf25bdb04fb6a
David Levy <dlevy4@asu.edu>
2024-02-27 14:28:58 -0700

    Refactored Block class to make it more php-like.

--- web/modules/webspark/webspark_view_modes/src/Plugin/views/style/Block.php
+++ web/modules/webspark/webspark_view_modes/src/Plugin/views/style/Block.php
@@ -1,23 +1,25 @@
 <?php
 
+//I like to define what Types to expect.. sue me.
 declare(strict_types=1);
 
-//Importing Enums to use for properties that have limited options to choose from
-require 'CardGroupType.php';
-require 'ViewModeType.php';
-require 'ColumnType.php';
-require 'SpacingType.php';
+//Importing Enums to use for props that have limited options to choose from to prevent against typos
+require 'enums/CardGroup.enum.php';
+require 'enums/ViewMode.enum.php';
+require 'enums/Column.enum.php';
+require 'enums/Spacing.enum.php';
+require 'enums/Color.enum.php';
 
 class Block {
 
-  //Each block should have a unique identifier
-  private string $uid;
+  //Each block should have a unique identifier, but not be able to explicitly set it
+  protected string $uid;
 
   //Some basic block properties
-  public string $block_title, $block_heading, $block_body;
-  public bool $block_isDisplaying_title;
+  public string $block_title, $block_heading, $block_body; //some strings
+  public bool $block_isDisplaying_title; //a bool
 
-  //Certain properties have a limited number of options, which lends itself to custom Enums
+  //Certain properties have a limited number of options, which I would think lends itself to using Enums
   public ColorEnum $block_text_color;
   public CardGroupEnum $block_card_group_type;
   public SpacingEnum $spacing_bottom;
@@ -29,22 +31,24 @@ class Block {
   //TODO:  Discuss the option to associate cards by using a collection of Card UIDs instead of Card objects.
   public array $cards = array();
 
+  //Construct a Block with key/value arguments, or else use default values defined in the Ternary operators below (??)
+  public function __construct($args=[]) {
 
+    $this->uid = $args['uid'] ?? uniqid(); //if there is no existing uid: generate one.
+    $this->block_title = $args['block_title'] ?? '';
+    $this->block_heading = $args['block_heading'] ?? '';
+    $this->block_body = $args['block_body'] ?? '';
+    $this->block_isDisplaying_title = $args['block_isDisplaying'] ?? '';
+    $this->block_text_color = $args['block_text_color'] ?? '';
+    $this->block_card_group_type = $args['block_card_group_type'] ?? '';
+    $this->spacing_bottom = $args['spacing_bottom'] ?? '';
+    $this->spacing_top = $args['spacing_top'] ?? '';
+    $this->view_mode_type = $args['view_mode_type'] ?? '';
+    $this->column_type = $args['column_type'] ?? '';
+  }
 
-  // When constructing a Block if arguments are not passed in, generate a new one with the default paramaters as defined in the constructor argument.
-  public function __construct($uid = "", $block_title = "", $block_heading = "", $block_body = "", $block_isDisplaying_title = false, $block_text_color = ColorEnum::DEFAULT, $block_card_group_type = CardGroupEnum::DEFAULT, $spacing_bottom = SpacingEnum::NONE, $spacing_top = SpacingEnum::NONE, $view_mode_type = ViewModeEnum::DEFAULT, $column_type = ColumnEnum::SELECT) {
-
-    $this->uid = $uid ? $uid : uniqid(); //if there is no existing uid: generate one.
-    $this->block_title = $block_title;
-    $this->block_heading = $block_heading;
-    $this->block_body = $block_body;
-    $this->block_isDisplaying_title = $block_isDisplaying_title;
-    $this->block_text_color = $block_text_color;
-    $this->block_card_group_type = $block_card_group_type;
-    $this->spacing_bottom = $spacing_bottom;
-    $this->spacing_top = $spacing_top;
-    $this->view_mode_type = $view_mode_type;
-    $this->column_type = $column_type;
-
+  //This is a trick so they can take a look at the uid... look but no touch!
+  public function uid() {
+      return self::$uid;
   }
 }
