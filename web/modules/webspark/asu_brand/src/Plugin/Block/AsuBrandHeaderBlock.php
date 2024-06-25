68f5b354e7cceadfe12b5497b7a50e8367e1eb6f
Bob-E <rmlebla1@asu.edu>
2024-06-24 18:53:24 -0700

    update Parent unit fields in AsuBrandHeaderBlock.php and WebsparkConfigureHeaderForm.php

--- web/modules/webspark/asu_brand/src/Plugin/Block/AsuBrandHeaderBlock.php
+++ web/modules/webspark/asu_brand/src/Plugin/Block/AsuBrandHeaderBlock.php
@@ -208,20 +208,29 @@ class AsuBrandHeaderBlock extends BlockBase {
       '#title' => $this->t('Parent unit name'),
       '#description' => $this->t("(optional) Name of the ASU parent unit. Will appear above the site title. Leave blank if none."),
       '#default_value' => $config['asu_brand_header_block_parent_org'] ?? '',
-      '#states' => array(
-        'required' => array(
-          ':input[name="settings[titles][asu_brand_header_block_parent_org_url]"]' => array(
-            'filled' => TRUE,
-          ),
-        ),
-      ),
+      '#states' => [
+        'required' => [
+          ':input[name="settings[titles][asu_brand_header_block_parent_org_url]"]' => ['filled' => TRUE],
+        ],
+      ],
     ];
+
     $form['titles']['asu_brand_header_block_parent_org_url'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Parent department URL'),
-      '#description' => $this->t('Absolute or relative URL of the parent unit. Leave blank if none.'),
+      '#description' => $this->t('Absolute or relative URL of the parent unit.'),
       '#default_value' => $config['asu_brand_header_block_parent_org_url'] ?? '',
+      '#states' => [
+        'visible' => [
+          ':input[name="settings[titles][asu_brand_header_block_parent_org]"]' => ['filled' => TRUE],
+        ],
+        'required' => [
+          ':input[name="settings[titles][asu_brand_header_block_parent_org]"]' => ['filled' => TRUE],
+        ],
+      ],
     ];
+
+
 
 
     $form['menus'] = array(
