a0a29713e62641bf00620208751bde27b0bb6aff
Alexander Persky <5995907+bebaps@users.noreply.github.com>
2024-11-15 14:43:12 -0700

    setup homepage test

--- test/playwright/tests/homepage.spec.js
+++ test/playwright/tests/homepage.spec.js
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage', async ({ page }) => {
-  await page.goto('/');
-
-  await expect(page.getByTestId('logo')).toBeVisible();
-  await expect(page.getByRole('heading')).toContainText('Explore Webspark 2');
+test.describe('Homepage tests', { tag: '@webspark' }, () => {
+  test('homepage loads', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.locator('#asuHeader')).toBeVisible();
+    await expect(page.getByRole('heading')).toContainText('Explore Webspark 2');
+    await expect(page.locator('#asu-footer')).toBeVisible();
+  });
 });
