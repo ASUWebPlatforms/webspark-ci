e36ce0a663ac0ac1e5fe068a87ae9090e7e21c8d
Alexander Persky <5995907+bebaps@users.noreply.github.com>
2024-11-15 14:43:22 -0700

    setup video test

---
+++ test/playwright/tests/video.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Video tests', { tag: '@webspark' }, () => {
+  test.beforeEach('setup', async ({ page }) => {
+    await page.goto('/video');
+  });
+
+  test('video loads', async ({ page }) => {
+    await expect(page.locator('iframe[title="🌴 Tropical Beach Ambience on a Island in Thailand with Ocean Sounds For Relaxation \\& Holiday Feeling"]').contentFrame().locator('iframe[title="🌴 Tropical Beach Ambience on a Island in Thailand with Ocean Sounds For Relaxation \\& Holiday Feeling"]').contentFrame().locator('.ytp-cued-thumbnail-overlay-image')).toBeVisible();
+  });
+
+  test('video plays', async ({ page }) => {
+    await page.locator('iframe[title="🌴 Tropical Beach Ambience on a Island in Thailand with Ocean Sounds For Relaxation \\& Holiday Feeling"]').contentFrame().locator('iframe[title="🌴 Tropical Beach Ambience on a Island in Thailand with Ocean Sounds For Relaxation \\& Holiday Feeling"]').contentFrame().getByLabel('Play', { exact: true }).click();
+  });
+});
