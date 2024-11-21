7fdf888a7b5525157f0cba0581b2847ba596d178
Alexander Persky <5995907+bebaps@users.noreply.github.com>
2024-11-21 12:55:13 -0700

    add ckeditor test

---
+++ test/playwright/tests/ckeditor.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import drupal from './helpers/drupal.helpers';
+
+const buttons = [
+  'Bold',
+  'Italic',
+  'Link',
+  'Bulleted List',
+  'Numbered List',
+  'List Properties',
+  'Insert Fontawesome Icon',
+  'Upload image from computer',
+  'Insert Media',
+  'Heading',
+  'Source',
+  'CKEditor Responsive',
+  'Button',
+  'Horizontal line',
+  'Divider',
+  'Lead',
+  'Highlighted Heading',
+  'Blockquote',
+  'Blockquote Animated',
+  'Webspark table',
+];
+
+test.describe('ckeditor tests', { tag: '@webspark' }, () => {
+  test('default buttons load', async ({ page }) => {
+    await drupal.loginAsAdmin(page);
+    await page.goto('/node/add/page');
+    await expect(page.getByLabel('Highlight').nth(1)).toBeVisible();
+    for (const i of buttons) {
+      await expect(page.getByLabel(i, { exact: true })).toBeVisible();
+    }
+  });
+});
