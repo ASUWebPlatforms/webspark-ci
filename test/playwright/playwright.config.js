bd95d7204cb2c65b34a11290c25670e8c67c7573
Alexander Persky <apersky@asu.edu>
2025-02-25 13:45:19 -0700

    fail fast

--- test/playwright/playwright.config.js
+++ test/playwright/playwright.config.js
@@ -24,6 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Fail fast */
+  maxFailures: process.env.CI ? 1 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
