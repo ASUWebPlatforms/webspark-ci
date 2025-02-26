f7c152df00c24f64308b4c39abf9ac24dd25a5c8
Alexander Persky <apersky@asu.edu>
2025-02-26 10:56:07 -0700

    update the 404 test

--- test/playwright/tests/pages/404.spec.js
+++ test/playwright/tests/pages/404.spec.js
@@ -1,7 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('404', { tag: ['@webspark', '@desktop'] }, async ({ page }) => {
-  await page.goto('/nonexistent');
-  await expect(page.getByRole('heading', { name: 'Hmm, we can\'t find that page' })).toBeVisible();
-  await expect(page.locator('h1')).toContainText('Hmm, we can\'t find that page');
+test.describe('404 page tests', { tag: ['@webspark', '@desktop'] }, () => {
+  test('verify', async ({ page }) => {
+    const response = await page.goto('/foo-bar');
+    const content = page.getByRole('heading', { name: 'Hmm, we can\'t find that page' });
+    const title = page.getByRole('heading', { name: 'Search all of ASU' });
+    const input = page.getByRole('textbox', { name: 'Search asu.edu' });
+
+    expect(response.status()).toBe(404);
+    await expect(content).toBeVisible();
+    await expect(title).toBeVisible();
+    await expect(input).toBeVisible();
+    await input.fill('sparky');
+    await page.evaluate(() => {
+      document.getElementById('webspark-blocks-asu-search-form').submit();
+    });
+    await page.waitForURL('https://search.asu.edu/**');
+
+    const finalUrl = page.url();
+    expect(finalUrl).toContain('https://search.asu.edu/');
+    expect(finalUrl).toContain('q=sparky');
+  });
 });
