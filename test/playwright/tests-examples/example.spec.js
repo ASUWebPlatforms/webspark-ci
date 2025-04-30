1f84200e9214e5c47ee1d84b2f907e1387ccfaaa
Alexander Persky <apersky@asu.edu>
2025-04-30 11:23:39 -0700

    Provide better documentation for the example test

--- test/playwright/tests-examples/example.spec.js
+++ test/playwright/tests-examples/example.spec.js
@@ -3,24 +3,45 @@ import drupal from '../helpers/drupal';
 
 const BLOCK = '';
 
+/**
+ * The overall test suite.
+ * Include the proper tags for ease of filtering.
+ *
+ * @type {import('@playwright/test').TestCase}
+ */
 test.describe(`${BLOCK} block tests`, { tag: ['@webspark', '@desktop', '@blocks', '@example'] }, () => {
   /** @type {import('@playwright/test').Page} */
   let page;
   let pageUrl;
 
+  /**
+   * Runs before all tests in the suite.
+   * Use this to set up the environment as a whole.
+   */
   test.beforeAll('setup', async ({ browser }) => {
     page = await browser.newPage();
     pageUrl = await drupal.createPage(page, BLOCK);
   });
 
+  /**
+   * Runs before each individual test in the suite.
+   * Use this for repeating setup steps for each test.
+   */
   test.beforeEach(async () => {
     await page.goto(pageUrl);
   });
 
+  /**
+   * Runs after all tests in the suite have been completed.
+   * Use this to clean up the environment as a whole.
+   */
   test.afterAll('cleanup', async () => {
     await page.close();
   });
 
+  /**
+   * Create the block to be tested.
+   */
   test('create', async () => {
     await page.getByRole('link', { name: 'Layout' }).click();
     await drupal.addBlock(page, BLOCK);
@@ -33,11 +54,24 @@ test.describe(`${BLOCK} block tests`, { tag: ['@webspark', '@desktop', '@blocks'
     await page.getByRole('button', { name: 'Save layout' }).click();
   });
 
+  /**
+   * Verify that the block is displayed correctly.
+   */
   test('verify', async () => {
     // ...
   });
 
-  test('data layer', async () => {
+  /**
+   * Verify that the block meets accessibility standards.
+   */
+  test('verify (a11y)', async () => {
+    // ...
+  });
+
+  /**
+   * Verify that the block meets Data Layer standards.
+   */
+  test('verify (data layer)', async () => {
     // ...
   });
 });
