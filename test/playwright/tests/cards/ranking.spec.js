f3bd3b58c1b68413e89c78f5663382a8ca161e3b
Alexander Persky <apersky@asu.edu>
2025-04-30 11:23:21 -0700

    Scaffold other card tests

--- test/playwright/tests/cards/ranking.spec.js
+++ test/playwright/tests/cards/ranking.spec.js
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 import drupal from '../helpers/drupal';
 
 const BLOCK = 'Card Arrangement';
-const CARD = 'Ranking';
+const TYPE = 'Ranking';
 
-test.describe(`${CARD} card tests`, { tag: ['@webspark', '@desktop', '@cards'] }, () => {
+test.describe(`${BLOCK} (${TYPE}) tests`, { tag: ['@webspark', '@desktop', '@cards'] }, () => {
   /** @type {import('@playwright/test').Page} */
   let page;
   let pageUrl;
@@ -14,7 +14,7 @@ test.describe(`${CARD} card tests`, { tag: ['@webspark', '@desktop', '@cards'] }
     await drupal.consent(page);
     await drupal.toggleUniversalGTM();
     await drupal.loginAsAdmin(page);
-    pageUrl = await drupal.createPage(page, BLOCK);
+    pageUrl = await drupal.createPage(page, `${BLOCK} (${TYPE})`);
   });
 
   test.beforeEach(async () => {
@@ -30,19 +30,12 @@ test.describe(`${CARD} card tests`, { tag: ['@webspark', '@desktop', '@cards'] }
     await drupal.addBlock(page, BLOCK);
 
     //--- Begin custom test steps
-    // await page.getByRole('button', { name: 'Cards' }).click();
-    // await page.getByRole('button', { name: 'Add Card Group Ranking' }).click();
-    // await page.getByRole('textbox', { name: 'Required Heading' }).click();
-    // await page.getByRole('textbox', { name: 'Required Heading' }).fill('Card headihng');
-    // await page.getByLabel('Required Card Ranking Image').selectOption('small');
-    // await page.getByRole('button', { name: 'Add media' }).click();
-    // await page.getByRole('checkbox', { name: 'Select Hero-DreamscapeLearn-' }).check();
-    // await page.getByRole('button', { name: 'Insert selected' }).click();
-    // await page.getByRole('textbox', { name: 'Citation' }).click();
-    // await page.getByRole('textbox', { name: 'Citation' }).fill('Card citation');
-    // await page.getByRole('textbox', { name: 'Editor editing area: main.' }).getByRole('paragraph').click();
-    // await page.getByRole('textbox', { name: 'Link' }).click();
-    // await page.getByRole('textbox', { name: 'Link' }).fill('https://asu.edu');
+    await page.getByLabel('Required Columns to Display').selectOption({ label: 'Two Columns' });
+    await page.getByRole('button', { name: 'Cards' }).click();
+
+    // Add Cards
+    await page.getByRole('button', { name: 'Add Card Group Ranking' }).click();
+    await page.waitForTimeout(1000);
     //--- End custom test steps
 
     await page.getByRole('button', { name: 'Add block' }).click();
@@ -50,15 +43,6 @@ test.describe(`${CARD} card tests`, { tag: ['@webspark', '@desktop', '@cards'] }
   });
 
   test('verify', async () => {
-    // .card-ranking to have count 1
-    // await expect(page.getByRole('img', { name: 'test' })).toBeVisible();
-    // await expect(page.getByRole('heading', { name: 'Card headihng' })).toBeVisible();
-    // await expect(page.getByText('— Card citation')).toBeVisible();
-    // await expect(page.getByText('Card content')).toBeVisible();
-    // await expect(page.getByRole('button', { name: 'Card headihng' })).toBeVisible();
-    // .card-ranking .info-layer to have css 'border-top: 0.5rem solid #ffc627'
-    // await expect(page.getByRole('link', { name: 'Read more Card headihng' })).toBeHidden();
-    // await page.getByRole('button', { name: 'Card headihng' }).click();
-    // await expect(page.getByRole('link', { name: 'Read more Card headihng' })).toBeVisible();
+
   });
 });
---
+++ test/playwright/tests/cards/story.spec.js
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+import drupal from '../helpers/drupal';
+
+const BLOCK = 'Card Arrangement';
+const TYPE = 'Story';
+
+test.describe(`${BLOCK} (${TYPE}) tests`, { tag: ['@webspark', '@desktop', '@cards'] }, () => {
+  /** @type {import('@playwright/test').Page} */
+  let page;
+  let pageUrl;
+
+  test.beforeAll('setup', async ({ browser }) => {
+    page = await browser.newPage();
+    await drupal.consent(page);
+    await drupal.toggleUniversalGTM();
+    await drupal.loginAsAdmin(page);
+    pageUrl = await drupal.createPage(page, `${BLOCK} (${TYPE})`);
+  });
+
+  test.beforeEach(async () => {
+    await page.goto(pageUrl);
+  });
+
+  test.afterAll('cleanup', async () => {
+    await page.close();
+  });
+
+  test('create', async () => {
+    await page.getByRole('link', { name: 'Layout' }).click();
+    await drupal.addBlock(page, BLOCK);
+
+    //--- Begin custom test steps
+    await page.getByLabel('Required Columns to Display').selectOption({ label: 'Two Columns' });
+    await page.getByRole('button', { name: 'Cards' }).click();
+
+    // Add Cards
+    await page.getByRole('button', { name: 'Add Card Group Story' }).click();
+    await page.waitForTimeout(1000);
+    //--- End custom test steps
+
+    await page.getByRole('button', { name: 'Add block' }).click();
+    await page.getByRole('button', { name: 'Save layout' }).click();
+  });
+
+  test('verify', async () => {
+
+  });
+});
