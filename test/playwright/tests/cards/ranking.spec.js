bfc0c88bd0d921138d6f9729d9aaa9f0ad8b0186
Alexander Persky <apersky@asu.edu>
2025-04-30 14:59:10 -0700

    Add Ranking Card test

--- test/playwright/tests/cards/ranking.spec.js
+++ test/playwright/tests/cards/ranking.spec.js
@@ -36,6 +36,19 @@ test.describe(`${BLOCK} (${TYPE}) tests`, { tag: ['@webspark', '@desktop', '@car
     // Add Cards
     await page.getByRole('button', { name: 'Add Card Group Ranking' }).click();
     await page.waitForTimeout(1000);
+    await page.getByRole('textbox', { name: 'Required Heading' }).fill('Card heading');
+    await page.getByRole('combobox', { name: 'Required Card Ranking Image Size' }).selectOption({ label: 'Small' });
+    await drupal.addMediaField(page);
+    await page.getByRole('textbox', { name: 'Citation' }).fill('Card citation');
+    await page.getByLabel('Rich Text Editor').getByRole('textbox').fill('Card content');
+    await page.getByRole('textbox', { name: 'Link' }).fill('https://asu.edu');
+
+    await page.getByRole('button', { name: 'Add Card Ranking' }).click();
+    await page.getByRole('textbox', { name: 'Required Heading' }).nth(1).fill('Card heading 2');
+    await page.getByRole('combobox', { name: 'Required Card Ranking Image Size' }).nth(1).selectOption({ label: 'Large' });
+    await drupal.addMediaField(page, 1);
+    await page.getByLabel('Rich Text Editor').getByRole('textbox').nth(1).fill('Card content 2');
+    await page.getByRole('textbox', { name: 'Link' }).nth(1).fill('https://asu.edu');
     //--- End custom test steps
 
     await page.getByRole('button', { name: 'Add block' }).click();
@@ -43,6 +56,31 @@ test.describe(`${BLOCK} (${TYPE}) tests`, { tag: ['@webspark', '@desktop', '@car
   });
 
   test('verify', async () => {
+    const cards = page.locator('.card-ranking');
+    const imgs = page.locator('.uds-img');
+    const heading = page.getByRole('heading', { name: 'Card heading', exact: true });
+    const btn = page.getByRole('button', { name: 'Card heading', exact: true });
+    const heading2 = page.getByRole('button', { name: 'Card heading 2', exact: true });
+    const citation = page.getByText('Card citation');
+    const content = page.getByText('Card content', { exact: true });
+    const content2 = page.getByText('Card content 2', { exact: true });
+    const link = page.getByRole('link', { name: 'Read more Card heading', exact: true });
 
+    await expect(cards.nth(0)).toHaveClass(/small-image/);
+    await expect(cards.nth(1)).toHaveClass(/large-image/);
+    await expect(imgs).toHaveCount(2);
+    await expect(heading).toBeVisible();
+    await expect(heading2).toBeVisible();
+    await expect(citation).toBeVisible();
+    await expect(content).toBeVisible();
+    await expect(content2).toBeHidden();
+
+    // Interactions
+    await expect(link).toBeHidden();
+    await btn.click();
+    await expect(link).toBeVisible();
+    await expect(link).toHaveAttribute('href', 'https://asu.edu');
+    await heading2.click();
+    await expect(content2).toBeVisible();
   });
 });
