693452528e4e75afcd23f34e3d6f9c976d7c88a1
Alexander Persky <apersky@asu.edu>
2025-06-04 16:18:24 -0700

    Scaffold the Degree RFI component test

---
+++ test/playwright/tests/pages/degree-rfi.spec.js
@@ -0,0 +1,80 @@
+import { test, expect } from '@playwright/test';
+import drupal from '../helpers/drupal';
+
+test.describe('degree rfi tests', { tag: ['@webspark', '@desktop', '@pages'] }, () => {
+  /** @type {import('@playwright/test').Page} */
+  let page;
+  let pageUrl;
+
+  test.beforeAll('setup', async ({ browser }) => {
+    page = await browser.newPage();
+    await drupal.consent(page);
+    await drupal.toggleUniversalGTM();
+    await drupal.loginAsAdmin(page);
+    await drupal.enableModule('asu_degree_rfi');
+    pageUrl = await drupal.createPage(page, 'Degree RFI');
+  });
+
+  test.beforeEach(async () => {
+    await page.goto(pageUrl);
+  });
+
+  test.afterAll('cleanup', async () => {
+    await page.close();
+  });
+
+  test('create', async () => {
+    await page.getByRole('link', { name: 'Layout' }).click();
+    await page.getByRole('link', { name: 'Add block in Content, First region' }).click();
+    await page.getByRole('link', { name: 'RFI form component' }).click();
+
+    //--- Begin custom test steps
+    await page.waitForTimeout(3000);
+    await page.getByLabel('RFI form type variation').selectOption('rfiVariant1');
+    await page.getByRole('checkbox', { name: 'Run in test mode' }).check();
+    //--- End custom test steps
+
+    await page.getByRole('button', { name: 'Add block' }).click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Save layout' }).click();
+  });
+
+  // Testing against a React component so we only need to
+  // esnsure the data is passed to the props
+  test('verify', async () => {
+    // Check for the user inputs to be submitted
+    await page.waitForLoadState();
+    const props = await page.evaluate(() => {
+      return window.drupalSettings.asu_degree_rfi.props;
+    });
+
+    await expect(props).toBeDefined();
+    expect(typeof props).toBe('object');
+
+    try {
+      await expect(props.test).toBe(1);
+    } catch (error) {
+      console.error('Form is not in test mode! Forcefully closing the page.');
+      await page.close();
+      throw error;
+    }
+
+    // Complete the form
+    await page.getByLabel('Which applies to you?').selectOption('ONLNE');
+    // await page.getByLabel('Select your student status')).selectOption('First Time Freshman');
+    // await page.getByLabel('Area of interest')).selectOption('Business');
+    await page.getByLabel('Program of interest').selectOption('UGBA-BABUSTBA');
+    await page.getByRole('textbox', { name: 'Email Address' }).fill('email@email.com');
+    await page.getByRole('textbox', { name: 'First name' }).fill('Playwright');
+    await page.getByRole('textbox', { name: 'Last name' }).fill('User');
+    await page.getByRole('textbox', { name: '1 (702) 123-' }).fill('+1 (555) 555-5555');
+    await page.getByText('I consent', { exact: true }).click();
+
+    // Intercept the form and verify data
+    const textContent = page.getByText('{ "values": { "GdprConsent":').textContent();
+    // TOOD: Turn this into a valid JSON object, need the string to be valid format first
+    const data = JSON.parse(textContent);
+    expect(typeof data.values).toBe('object');
+    console.log(data.values);
+  });
+});
